562f73030d33b960d9602dee0c592a63541917f6
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-10-12 04:02:29 +0900

    Feat: SafeArea 동적 적용

--- App.tsx
+++ App.tsx
@@ -1,18 +1,77 @@
-import { StatusBar } from "expo-status-bar";
-import { Fragment } from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import WebView from "react-native-webview";
+import WebView, { WebViewMessageEvent } from "react-native-webview";
+import { useState } from "react";
+import { match } from "ts-pattern";
+import { View } from "react-native";
+import {
+  Edge,
+  SafeAreaProvider,
+  SafeAreaView,
+} from "react-native-safe-area-context";
 
 export default function App() {
+  const [safeMode, setSafeMode] = useState<"top" | "bottom" | "all" | "none">(
+    "all"
+  );
+  const [topBackground, setTopBackground] = useState("#F1F1E7");
+  const [bottomBackground, setBottomBackground] = useState("#FFFFFF");
+
+  const handleMessage = (event: WebViewMessageEvent) => {
+    const { type, payload } = JSON.parse(event.nativeEvent.data);
+    switch (type) {
+      case "setSafeMode":
+        setSafeMode(payload);
+        match(payload as "top" | "bottom" | "all")
+          .with("all", () => {
+            setTopBackground("#F1F1E7");
+            setBottomBackground("#FFFFFF");
+          })
+          .with("top", () => {
+            setTopBackground("#F1F1E7");
+          })
+          .with("bottom", () => {
+            setBottomBackground("#FFFFFF");
+          });
+        break;
+      case "setTopBackground":
+        setTopBackground(payload);
+        break;
+      case "setBottomBackground":
+        setBottomBackground(payload);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <Fragment>
-      <SafeAreaView style={{ flex: 0, backgroundColor: "#F1F1E7" }} />
-      <SafeAreaView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
-        <WebView
-          style={{ flex: 1 }}
-          source={{ uri: "https://refri-webview.vercel.app/" }}
-        />
-      </SafeAreaView>
-    </Fragment>
+    <SafeAreaProvider>
+      {match(safeMode)
+        .with("all", "top", () => (
+          <SafeAreaView
+            edges={["top"]}
+            style={{ backgroundColor: topBackground }}
+          />
+        ))
+        .otherwise(() => null)}
+
+      <WebView
+        source={{
+          uri: "https://refri-webview.vercel.app",
+        }}
+        textZoom={100}
+        onMessage={handleMessage}
+        decelerationRate="normal"
+        allowsBackForwardNavigationGestures
+      />
+
+      {match(safeMode)
+        .with("all", "bottom", () => (
+          <SafeAreaView
+            edges={["bottom"]}
+            style={{ backgroundColor: bottomBackground }}
+          />
+        ))
+        .otherwise(() => null)}
+    </SafeAreaProvider>
   );
 }
